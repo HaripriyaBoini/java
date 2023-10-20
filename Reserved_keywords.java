7082c9b009bd55c11005373152f52a86cd750702
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-10-20 16:35:06 +0530

    Add files via upload

---
+++ Reserved_keywords.java
@@ -0,0 +1,15 @@
+package Corejavaelements;
+
+public class Reserved_keywords {
+
+	public static void main(String[] args) {
+		System.out.println("java is a freeform language");
+		boolean ispassed = true;    //true,false=keywords
+		boolean isfail = false;
+		System.out.println("x is passed: " +ispassed); //reserved keywords are 61 two of them are true,false
+		System.out.println("x is passed: " +isfail);   //unused=goto,label
+		
+
+	}
+
+}
