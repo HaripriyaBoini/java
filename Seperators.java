a0bb1aa4747449ee47dbc0d797c723bc2cc6ee7b
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-10-27 21:02:19 +0530

    Add files via upload

---
+++ Seperators.java
@@ -0,0 +1,19 @@
+package Corejavaelements;
+
+public class Seperators {                     //{} = used to define classes
+
+	public static void main(String[] args) {   //() = used in method def,encloses exp'n)
+		int number=1,number2;                  // , = seperates variabled
+            System.out.println("enter the number:" +number);	
+            switch(number) {
+            case 1:                             //: == used in break,continue,cases
+            	System.out.println("passed");    //. = used to refer var&class method
+            	break;
+            case 2:
+            	System.out.println("failed");    //; = terminate the stmt
+ 
+            }	
+
+	}
+
+}
