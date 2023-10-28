9c884fedaaa842348be1b917a5a53c905f207bd7
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-10-29 00:04:24 +0530

    Add files via upload

---
+++ Do_while_stmt.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Do_while_stmt {
+
+	public static void main(String[] args) {
+		int i=1;
+		do
+		{
+             System.out.println("i="+i);
+             i++;
+        }
+		while(i<=10);
+	   
+
+	}
+
+}
