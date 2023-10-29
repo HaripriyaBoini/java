6e08f1332ca80512bff44ffb222dd4181faf323a
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-10-29 23:13:23 +0530

    Add files via upload

--- Labeled_Break.java
+++ Labeled_Break.java
@@ -3,14 +3,14 @@ package jump;
 public class Labeled_Break {
 
 	public static void main(String[] args) {
-		for(int i=0;i<3;i++) {
+		for(int i=1;i<=3;i++) {
 			System.out.println("\n i="+i);
 		}
 		one:
-			for(int j=0;j<3;j++)
+			for(int j=1;j<=3;j++)
 			{
 				if(j==2)
-					break one;
+					break one;                  //the labels will break only if there is a break stmt
 				System.out.println("j="+j);
 			}
 	}
