44d90c56a364e24dc42bbc2fb0f15aeb0de0a7a1
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-10-29 23:16:22 +0530

    Add files via upload

---
+++ PrintPattern2.java
@@ -0,0 +1,16 @@
+package Assignments_of_ContrlStmts;
+
+public class PrintPattern2 {
+	public static void main(String[] arg) {
+		for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+		
+	
+
+
