81bd5d1bce1cd11784c10ef292a9bb705354815f
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-02 14:56:00 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,18 +1,13 @@
-package variables;
+package variable.instance;
 
 public class Employee {
-
-                 double getTotalSalary(double basicPay,double hra,double da) {
-                	 double totalSalary; 
-                	 if(basicPay>=10000) {
-                		 totalSalary=basicPay+hra+da;
-                	 }
-                	 else {
-                		 totalSalary=basicPay+hra+da+5000;
-                	 }
-                
-                     return totalSalary;
+	double basicPay;
+	double hra;
+	double da;
+	double getTotalSalary() {
+		return basicPay+hra+da;
 	}
+	 
+		
+
 }
-
-
