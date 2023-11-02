3c279801ae07e4b1360c476da104aa9fcda06008
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-02 14:56:42 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,13 +1,12 @@
-package variable.instance;
+package variables_static;
 
 public class Employee {
 	double basicPay;
 	double hra;
 	double da;
+	static String companyName="uno career";
 	double getTotalSalary() {
 		return basicPay+hra+da;
 	}
-	 
-		
 
 }
