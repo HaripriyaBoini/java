f0892824539c5a4dc6003312c77280769659e511
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-02 15:01:27 +0530

    Add files via upload

---
+++ ConstructorDemo_java.java
@@ -0,0 +1,24 @@
+package constructor;
+
+public class ConstructorDemo_java {
+
+	public static void main(String[] args) {
+		
+			Box matchBox=new Box();
+			System.out.println(matchBox.length);
+			System.out.println(matchBox.width);
+			System.out.println(matchBox.height);
+			
+			Box shoeBox=new Box(6,5,4);
+				System.out.println(shoeBox.length);
+				System.out.println(shoeBox.width);
+				System.out.println(shoeBox.height);
+				
+				Box squareBox=new Box(5,5,5);
+				System.out.println(squareBox.length);
+				System.out.println(squareBox.width);
+				System.out.println(squareBox.height);
+				
+	}
+
+}
