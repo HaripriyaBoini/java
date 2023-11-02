c24239298d4f1368d6cc602fcfcc9c55165b1c4d
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-02 14:59:04 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,12 +1,25 @@
-package variables_static;
+package Methods;
 
 public class Employee {
 	double basicPay;
 	double hra;
 	double da;
-	static String companyName="uno career";
+	
 	double getTotalSalary() {
 		return basicPay+hra+da;
+	}
+	double getTotalSalarywithIncentive(double incentive) {
+		return basicPay+hra+da+incentive;
+	}
+	void displayValues() {
+		System.out.println("basicpay"+basicPay);
+		System.out.println("hr"+hra);
+		System.out.println("da"+da);
+		System.out.println("total salary"+getTotalSalary());
+		System.out.println("with incentive"+getTotalSalarywithIncentive(10000));
+		
+		
+		
 	}
 
 }
