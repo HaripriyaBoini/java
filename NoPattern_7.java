c006a8522ca27a4b3e6899f18a212207f679fdef
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-22 16:43:12 +0530

    Add files via upload

---
+++ NoPattern_7.java
@@ -0,0 +1,22 @@
+package javaprgm;
+
+public class NoPattern_7 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1 2 3 4 5
+		 * 1 2 3 4
+		 * 1 2 3
+		 * 1 2 
+		 * 1 	
+		 */
+		for(int r=1;r<=5;r++) {
+			for(int c=5;c<=r;c--) {
+				System.out.print(c + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
