7ac16e86ad31f7d3af22e61e36793e7da03c48f3
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-23 16:06:03 +0530

    Add files via upload

---
+++ No_Args.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class No_Args {
+	
+	// methods-- 4 types
+	// no args ...no return
+			
+			static void design(){
+				System.out.println("********");
+				System.out.println("--------");
+			}
+
+		public static void main(String[] args) {
+			
+			design();
+			System.out.println("Hello");
+			design();
+			System.out.println("Welcome");
+			design();
+
+
+	}
+
+}
