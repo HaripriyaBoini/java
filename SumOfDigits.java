28d6a07b6c2ac172ec8c8b88206bc9b053a2d3ac
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-24 14:50:38 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,25 @@
+package methods;
+
+public class SumOfDigits {
+
+	
+		
+		    public static void main(String[] args) {
+		        int number = 25;
+		        int sum = sumOfDigits(number);
+		        System.out.println("The sum is " + sum);
+		    }
+
+		    public static int sumOfDigits(int num) {
+		        int sum = 0;
+
+		         while (num != 0) {
+		            sum += num % 10; 
+		            num /= 10; 
+		            
+		        }
+		         
+		        return sum;
+	}
+
+}
