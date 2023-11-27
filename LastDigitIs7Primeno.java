f29fad4fc05bc9c8a4639a6e92e23443c46a5732
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-11-27 19:18:00 +0530

    Add files via upload

---
+++ LastDigitIs7Primeno.java
@@ -0,0 +1,72 @@
+package methods;
+
+public class LastDigitIs7Primeno {
+	static boolean toFindNumPrimeOrNot(int n){
+		int i=2;
+		boolean res = true;
+		
+		for(;(i<n/2) && (n % i != 0);i++);
+		
+		if(i < n/2) res = false;
+		return res;
+	}
+	
+
+	public static void main(String[] args) {
+	
+		
+		// last digit should be 7 which is prime number
+		
+		for(int i=200,count =0;i>=100;i--) {
+			if ( (toFindNumPrimeOrNot(i) == true) && ((i % 10 == 7))) {
+				
+				System.out.print(i+" ");
+				
+			}
+			}
+	}
+}
+
+
+
+
+
+/* 
+ * public class LastDigitIs7Primeno{
+ * static boolean toFindNumPrimeOrNot(int n){
+		int i=2;
+		boolean res = true;
+		
+		for(;(i<n/2) && (n % i != 0);i++);
+		
+		if(i < n/2) res = false;
+		return res;
+	}
+	public static void main(String[] args) {
+	
+		
+		// last digit should be 7 which is prime number
+		
+		for(int i=200,count =0,m=0,f=0,l=0;i>=100;i--) {
+          if ( toFindNumPrimeOrNot(i) == true) {
+				l= i % 10;
+				f = i / 100;
+				m = (i / 10) % 10;
+				if(m < (f+l))
+				System.out.print(i+" ");
+				
+				
+			}
+		}
+	
+	
+	
+	}
+
+	
+	
+}
+ */
+		
+
+
