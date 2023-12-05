43bd8a96095d80f5232e2e33defefdc7b83cd362
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-05 15:35:41 +0530

    Add files via upload

---
+++ Char_Array.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class Char_Array {
+
+	public static void main(String[] args) {
+		// short byte[] int[] long[] float[] double char String
+		// short[]
+		
+		char ch[];
+		ch = new char[10];
+		ch[0]='0';
+		ch[1]='1';
+		ch[2]='2';
+		ch[3]='3';
+		ch[4]='4';
+		ch[5]='a';
+		ch[6]='b';
+		ch[7]='c';
+		ch[8]='d';
+		ch[9]='e';
+		
+		for(int i=0;i<ch.length;i++) {
+			System.out.print(ch[i]+" ");
+		}
+
+
+	}
+
+}
