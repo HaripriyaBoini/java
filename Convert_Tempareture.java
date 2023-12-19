3ec95428141d4c45698946ef8a6ca8360ca6a25f
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:10:56 +0530

    Add files via upload

---
+++ Convert_Tempareture.java
@@ -0,0 +1,14 @@
+package corejavaprograms;
+
+public class Convert_Tempareture {
+
+	public static void main(String[] args) {
+		int fahrenheit1=100;
+		int celcius1;
+		System.out.println("Enter fahrenheit:"+fahrenheit1);
+		celcius1=(fahrenheit1-32)*5/9;
+		System.out.println("celcius1: "+celcius1);
+		
+	}
+
+}
