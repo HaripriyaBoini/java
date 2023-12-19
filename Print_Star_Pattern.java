d5f241b8ff6fc9595393ba564d66385ba39fe05f
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:57:35 +0530

    Add files via upload

---
+++ Print_Star_Pattern.java
@@ -0,0 +1,28 @@
+package Assignments_of_ContrlStmts;
+
+import java.util.Scanner;
+
+public class Print_Star_Pattern {
+	//    *
+   //     * *
+  //      * * *
+ //       * * * *
+//        * * * * *
+	public static void main(String[] args) {
+		int rows=5;
+	    for(int i=0; i<rows; i++)   
+	    {   
+	    for(int j=0; j<=i; j++)   
+	    {   
+	    System.out.print("* ");   
+	    }   
+	    System.out.println();   
+		
+	}
+	
+}
+}
+
+
+
+			
