d37996d2579a633ef3a226f77f22166ff53a1456
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:54:56 +0530

    Add files via upload

---
+++ Alphabet_pattern1.java
@@ -0,0 +1,23 @@
+package Assignments_of_ContrlStmts;
+
+import java.util.Scanner;
+
+public class Alphabet_pattern1 {
+
+	public static void main(String[] args) {
+/*
+                            A
+                          B A
+                        C B A
+                      D C B A
+                    E D C B A
+
+*/
+		
+			
+			}
+
+			}
+
+
+
