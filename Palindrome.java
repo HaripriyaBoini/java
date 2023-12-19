ff91d45a3d99c8fa82bd04ef533bf3c16d1f6513
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:57:01 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -3,29 +3,23 @@ package Assignments_of_ContrlStmts;
 public class Palindrome {
 
 	public static void main(String[] args) {
-		int n=12678;                     
+		int n=123321;               //12678                    
 		System.out.println("n="+n);
 		int rev=0;
 		int temp;
 		temp=n;
+		while(n!=0) {
 		rev =rev*10;
 		rev=rev+(n%10);
 		n=n/10;
-		if(temp==n)
+		}
+		
+		if(temp==rev)                             
 		System.out.println("palindrome is "+temp);
 		else
-			System.out.println("not palindrome");
+			System.out.println("not palindrome");      // not palindrome
 		
-		/*int n=123321;                     
-		System.out.println("n="+n);
-		int rev=0;
-		int temp;
-		temp=n;
-		rev =rev*10;
-		rev=rev+(n/10);
-		n=n/10;
-		System.out.println("palindrome is "+temp);
-		*/
+		
 	}
 
-}
+	}
