a86c29738a21e2b4895a285f50f675834d40ef52
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:57:17 +0530

    Add files via upload

---
+++ Print_No_pattern.java
@@ -0,0 +1,25 @@
+package Assignments_of_ContrlStmts;
+
+public class Print_No_pattern {
+	public static void main(String[] args) {
+		
+		int rows=5;
+		
+	    for(int i=rows;i>0; i--) {  //for(int i=5; i>0; i--) 
+	    	int k=11;
+	    	   
+	    	
+	    	for(int j=0;j<i;j++) {
+	    		
+	    		    System.out.print(k+" ");
+	    		     k=k+1;
+	    		
+	    	}
+	    	System.out.println();
+	    }
+	    
+	}
+}
+
+
+
