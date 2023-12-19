4f8733607f4335048446cb6de390a4178b96c110
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:59:26 +0530

    Add files via upload

--- ReverseDigits.java
+++ ReverseDigits.java
@@ -3,13 +3,16 @@ package Assignments_of_ContrlStmts;
 public class ReverseDigits {
 
 	public static void main(String[] args) {
-		int n=234;
-		int rev=0;
+		int n=1234;
+       int rev=0;
 System.out.println("before reverse:"+n);
 		while(n!=0) {
-		rev=rev*10;
-		rev=rev+(n%10);
-		n=n/10;}
+			
+		rev=rev*10;    //0       40       430
+		rev=rev+(n%10);//0+4=4   40+3=43  430+2
+		n=n/10;         //123      12      0
+		
+		}
 		 System.out.println("after reverse"+rev);
 	}
 }
