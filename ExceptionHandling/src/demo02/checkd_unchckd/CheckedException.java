54e62f904ddf9ed1e518015dacf6b75c8898728a
HaripriyaBoini <148444101+HaripriyaBoini@users.noreply.github.com>
2023-12-19 16:47:03 +0530

    Delete ExceptionHandling directory

--- ExceptionHandling/src/demo02/checkd_unchckd/CheckedException.java
+++
@@ -1,9 +0,0 @@
-package demo02.checkd_unchckd;
-
-public class CheckedException {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- ExceptionHandling/src/demo08/custom_excp/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package demo08.custom_excp;
-
-public class InsufficientFundsException extends Exception {
-	public InsufficientFundsException(int amount) {
-		System.out.println("sorry but you are short of"+amount+"Rs");
-	}
-}
